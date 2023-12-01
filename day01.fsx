8016922b76ae80e4790ab96a8c3eb5550f0b0f2b
Timo Vahtera <timo.vahtera@gmail.com>
2023-12-01 23:03:32 +0200

    Improve day 1

--- day01.fsx
+++ day01.fsx
@@ -1,22 +1,20 @@
 open System
 let input = IO.File.ReadAllLines "inputs/day01.txt"
 
-let solver input =
-    let rec foo acc (input : string) =
-        match input |> Seq.toList with
-        | []                          -> acc
-        | 'o'::'n'::'e'::_           -> foo $"{acc}1" input[1..]
-        | 't'::'w'::'o'::_           -> foo $"{acc}2" input[1..]
-        | 't'::'h'::'r'::'e'::'e'::_ -> foo $"{acc}3" input[1..]
-        | 'f'::'o'::'u'::'r'::_      -> foo $"{acc}4" input[1..]
-        | 'f'::'i'::'v'::'e'::_      -> foo $"{acc}5" input[1..]
-        | 's'::'i'::'x'::_           -> foo $"{acc}6" input[1..]
-        | 's'::'e'::'v'::'e'::'n'::_ -> foo $"{acc}7" input[1..]
-        | 'e'::'i'::'g'::'h'::'t'::_ -> foo $"{acc}8" input[1..]
-        | 'n'::'i'::'n'::'e'::_      -> foo $"{acc}9" input[1..]
-        | c::_ when Char.IsDigit c   -> foo $"{acc}{c}" input[1..]
-        | _::_                       -> foo acc input[1..]
-    input |> foo "" |> (fun c -> $"{Seq.head c}{Seq.last c}") |> int
+let rec solver acc (input : string) =
+    match input |> Seq.toList with
+    | []                          -> acc
+    | 'o'::'n'::'e'::_           -> solver $"{acc}1" input[1..]
+    | 't'::'w'::'o'::_           -> solver $"{acc}2" input[1..]
+    | 't'::'h'::'r'::'e'::'e'::_ -> solver $"{acc}3" input[1..]
+    | 'f'::'o'::'u'::'r'::_      -> solver $"{acc}4" input[1..]
+    | 'f'::'i'::'v'::'e'::_      -> solver $"{acc}5" input[1..]
+    | 's'::'i'::'x'::_           -> solver $"{acc}6" input[1..]
+    | 's'::'e'::'v'::'e'::'n'::_ -> solver $"{acc}7" input[1..]
+    | 'e'::'i'::'g'::'h'::'t'::_ -> solver $"{acc}8" input[1..]
+    | 'n'::'i'::'n'::'e'::_      -> solver $"{acc}9" input[1..]
+    | c::_ when Char.IsDigit c   -> solver $"{acc}{c}" input[1..]
+    | _::_                       -> solver acc input[1..]
 
 printfn "Part 1: %i" (input |> Array.sumBy (Seq.filter Char.IsDigit >> (fun c -> $"{Seq.head c}{Seq.last c}") >> int))
-printfn "Part 2: %i" (input |> Array.sumBy solver)
+printfn "Part 2: %i" (input |> Array.sumBy (solver "" >> (fun c -> $"{Seq.head c}{Seq.last c}") >> int))
