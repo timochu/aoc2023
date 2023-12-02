d8b13eaf9ffcf84fab2b8e268153626500f2e510
Timo Vahtera <timo.vahtera@gmail.com>
2023-12-02 13:59:02 +0200

    Day 2 improvements

--- day02.fsx
+++ day02.fsx
@@ -1,8 +1,8 @@
 open System
 let toGame (line : string) =
     let items = line.Split([| ": "; ", "; "; " |], StringSplitOptions.RemoveEmptyEntries)
-    let get (name : string) = items |> Array.filter (fun x -> x.Contains name) |> Array.map (Seq.filter Char.IsDigit >> String.Concat >> int)
-    get "Game" |> Seq.head, get "red" |> Seq.max, get "green" |> Seq.max, get "blue" |> Seq.max
+    let get (name : string) = items |> Array.filter (fun x -> x.Contains name) |> Array.map (Seq.filter Char.IsDigit >> String.Concat >> int) |> Array.max
+    get "Game", get "red", get "green", get "blue"
 
 let games = "inputs/day02.txt" |> IO.File.ReadAllLines |> Array.map toGame
 games |> Seq.sumBy (fun (id,r,g,b) -> if r < 13 && g < 14 && b < 15 then id else 0) |> printfn "Part 1: %A"
