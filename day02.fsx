e800f05b20c276cbb358000d3c75b00995c0380b
Timo Vahtera <timo.vahtera@gmail.com>
2023-12-02 12:40:37 +0200

    Day 2 formatting

--- day02.fsx
+++ day02.fsx
@@ -1,17 +1,15 @@
 open System
-let input = IO.File.ReadAllLines "inputs/day02.txt"
 
-let lineToGame (line : string) =
+let toGame (line : string) =
     let parts = line.Split([| "Game "; ": "; ", "; "; " |], StringSplitOptions.RemoveEmptyEntries)
     let count (color : string) = 
         parts |> Array.filter (fun x -> x.Contains color) |> Array.map (Seq.filter Char.IsDigit >> String.Concat >> int)
     (parts |> Array.head |> int, count "red", count "green", count "blue")
 
-let games = input |> Array.map lineToGame
+let games = IO.File.ReadAllLines "inputs/day02.txt" |> Array.map toGame
 
-games
-|> Array.filter (fun (_,r,g,b) -> r |> Array.forall ((>) 13) && g |> Array.forall ((>) 14) && b |> Array.forall ((>) 15)) 
-|> Array.sumBy (fun (id,_,_,_) -> id) |> printfn "Part 1: %A"
+printfn "Part 1: %A" 
+  (games |> Array.filter (fun (_,r,g,b) -> Array.forall ((>) 13) r &&  Array.forall ((>) 14) g && Array.forall ((>) 15) b) |> Array.sumBy (fun (id,_,_,_) -> id))
 
-games 
-|> Array.sumBy (fun (_,r,g,b) -> Array.max r * Array.max g * Array.max b) |> printfn "Part 2: %A"
+printfn "Part 2: %A"
+  (games |> Array.sumBy (fun (_,r,g,b) -> Array.max r * Array.max g * Array.max b))
