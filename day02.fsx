2bf7a939135a55fdfeae305a140e5820b0ace309
Timo Vahtera <timo.vahtera@gmail.com>
2023-12-02 13:47:44 +0200

    Day 2 improvements

--- day02.fsx
+++ day02.fsx
@@ -2,8 +2,8 @@ open System
 let toGame (line : string) =
     let items = line.Split([| ": "; ", "; "; " |], StringSplitOptions.RemoveEmptyEntries)
     let get (name : string) = items |> Array.filter (fun x -> x.Contains name) |> Array.map (Seq.filter Char.IsDigit >> String.Concat >> int)
-    get "Game" |> Array.head, get "red", get "green", get "blue"
+    get "Game" |> Seq.head, get "red" |> Seq.max, get "green" |> Seq.max, get "blue" |> Seq.max
 
 let games = "inputs/day02.txt" |> IO.File.ReadAllLines |> Array.map toGame
-games |> Seq.filter (fun (_,r,g,b) -> Seq.forall ((>) 13) r && Seq.forall ((>) 14) g && Seq.forall ((>) 15) b) |> Seq.sumBy (fun (id,_,_,_) -> id) |> printfn "Part 1: %A"
-games |> Seq.sumBy (fun (_,r,g,b) -> Seq.max r * Seq.max g * Seq.max b) |> printfn "Part 2: %A"
+games |> Seq.filter (fun (_,r,g,b) -> r < 13 && g < 14 &&  b < 15) |> Seq.sumBy (fun (id,_,_,_) -> id) |> printfn "Part 1: %A"
+games |> Seq.sumBy (fun (_,r,g,b) -> r * g * b) |> printfn "Part 2: %A"
