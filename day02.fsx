4b7d49e77b127fdcab14f1e6bda9941fbc889245
Timo Vahtera <timo.vahtera@gmail.com>
2023-12-02 13:52:01 +0200

    Improve day 2 part 1

--- day02.fsx
+++ day02.fsx
@@ -5,5 +5,5 @@ let toGame (line : string) =
     get "Game" |> Seq.head, get "red" |> Seq.max, get "green" |> Seq.max, get "blue" |> Seq.max
 
 let games = "inputs/day02.txt" |> IO.File.ReadAllLines |> Array.map toGame
-games |> Seq.filter (fun (_,r,g,b) -> r < 13 && g < 14 &&  b < 15) |> Seq.sumBy (fun (id,_,_,_) -> id) |> printfn "Part 1: %A"
+games |> Seq.sumBy (fun (id,r,g,b) -> if r < 13 && g < 14 && b < 15 then id else 0) |> printfn "Part 1: %A"
 games |> Seq.sumBy (fun (_,r,g,b) -> r * g * b) |> printfn "Part 2: %A"
