08a4e8cd5585795250d314d02daa6b47c9d4c838
Timo Vahtera <timo.vahtera@gmail.com>
2023-12-02 13:27:24 +0200

    improve day 2

--- day02.fsx
+++ day02.fsx
@@ -1,9 +1,9 @@
+open System
 let toGame (line : string) =
-    let parts = line.Split([| "Game "; ": "; ", "; "; " |], System.StringSplitOptions.RemoveEmptyEntries)
-    let count (color : string) = 
-        parts |> Array.filter (fun x -> x.Contains color) |> Array.map (Seq.filter System.Char.IsDigit >> System.String.Concat >> int)
-    parts |> Array.head |> int, count "red", count "green", count "blue"
+    let items = line.Split([| ": "; ", "; "; " |], StringSplitOptions.RemoveEmptyEntries)
+    let get (name : string) = items |> Array.filter (fun x -> x.Contains name) |> Array.map (Seq.filter Char.IsDigit >> String.Concat >> int)
+    get "Game" |> Array.head, get "red", get "green", get "blue"
 
-let games = "inputs/day02.txt" |> System.IO.File.ReadAllLines |> Array.map toGame
-games |> Seq.filter (fun (_,r,g,b) -> Seq.forall ((>) 13) r && Seq.forall ((>) 14) g && Seq.forall ((>) 15) b) |> Seq.sumBy (fun (id,_,_,_) -> id) |> printfn "Part 1: %A" 
+let games = "inputs/day02.txt" |> IO.File.ReadAllLines |> Array.map toGame
+games |> Seq.filter (fun (_,r,g,b) -> Seq.forall ((>) 13) r && Seq.forall ((>) 14) g && Seq.forall ((>) 15) b) |> Seq.sumBy (fun (id,_,_,_) -> id) |> printfn "Part 1: %A"
 games |> Seq.sumBy (fun (_,r,g,b) -> Seq.max r * Seq.max g * Seq.max b) |> printfn "Part 2: %A"
