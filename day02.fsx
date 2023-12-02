9b6f7e20ada4ed5553aef10c01f4b1533c839a6b
Timo Vahtera <timo.vahtera@gmail.com>
2023-12-02 14:05:36 +0200

    Day 2 improvements

--- day02.fsx
+++ day02.fsx
@@ -1,7 +1,7 @@
-open System
+open System ; open type System.Char ; open type System.String
 let toGame (line : string) =
     let items = line.Split([| ": "; ", "; "; " |], StringSplitOptions.RemoveEmptyEntries)
-    let get (name : string) = items |> Array.filter _.Contains(name) |> Array.map (Seq.filter Char.IsDigit >> String.Concat >> int) |> Array.max
+    let get (name : string) = items |> Array.filter _.Contains(name) |> Array.map (Seq.filter IsDigit >> Concat >> int) |> Array.max
     get "Game", get "red", get "green", get "blue"
 
 let games = "inputs/day02.txt" |> IO.File.ReadAllLines |> Array.map toGame
