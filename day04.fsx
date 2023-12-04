dba5fd25f5d72223b09de2379f12875a6e70c0b7
Timo Vahtera <timo.vahtera@gmail.com>
2023-12-04 21:54:24 +0200

    Improve day 4 wording

--- day04.fsx
+++ day04.fsx
@@ -1,6 +1,6 @@
 open System; open type StringSplitOptions
 
-let cards = 
+let matches = 
     [for line in IO.File.ReadAllLines "inputs/day04.txt" ->
         1, line[10..].Split(' ', RemoveEmptyEntries) |> fun nr -> Set.intersect (Set nr[..9]) (Set nr[10..]) |> Set.count]
 
@@ -9,5 +9,5 @@ let rec solver total =
     | [] -> total
     | (count, head)::tail -> solver (total + count) ([for n, c in tail.[..head-1] -> n + count, c] @ tail.[head..])
 
-cards |> List.sumBy (fun (_, count) -> pown 2 (count - 1)) |> printfn "Part 1: %i"
-cards |> solver 0 |> printfn "Part 2: %i"
+matches |> List.sumBy (fun (_, count) -> pown 2 (count - 1)) |> printfn "Part 1: %i"
+matches |> solver 0 |> printfn "Part 2: %i"
