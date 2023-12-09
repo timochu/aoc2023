cea6d6fec92a848bc22ea35931490a186b875c5a
Timo Vahtera <timo.vahtera@gmail.com>
2023-12-09 13:23:25 +0200

    Improve day 9

--- day09.fsx
+++ day09.fsx
@@ -2,8 +2,8 @@ let sequences = [ for s in System.IO.File.ReadAllLines "inputs/day09.txt" -> s |
 
 let rec solve (accumulator : int list list) =
     match accumulator |> List.last |> List.pairwise |> List.map (fun (a, b) -> b - a) with
-    | s when s |> List.forall ((=) 0) -> accumulator @ [s] |> List.map List.last |> List.sum
-    | s -> solve (accumulator @ [s])
+    | s when s |> List.forall ((=) 0) -> accumulator @ [s] |> List.sumBy List.last
+    | s                               -> solve (accumulator @ [s])
 
 sequences |> List.sumBy (fun sequence -> solve [sequence]) |> printfn "Part 1: %i"
 sequences |> List.sumBy (fun sequence -> solve [List.rev sequence]) |> printfn "Part 2: %i"
