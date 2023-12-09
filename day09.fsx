0d898083687a1c9db882becf2c20b0c83131a6f4
Timo Vahtera <timo.vahtera@gmail.com>
2023-12-09 13:07:29 +0200

    Improve day 9

--- day09.fsx
+++ day09.fsx
@@ -4,8 +4,8 @@ let sequences = [ for s in IO.File.ReadAllLines "inputs/day09.txt" -> s |> _.Spl
 
 let rec differences (sequences : int list list) =
     match sequences |> List.last |> List.pairwise |> List.map (fun (a, b) -> b - a) with
-    | s when s |> List.forall ((=) 0) -> sequences @ [s]
+    | s when s |> List.forall ((=) 0) -> sequences @ [s] |> List.map List.last |> List.sum
     | s -> differences (sequences @ [s])
 
-printfn "Part 1: %A" (sequences |> List.sumBy (fun x -> differences [x] |> List.map List.last |> List.sum))
-printfn "Part 2: %A" (sequences |> List.sumBy (fun x -> differences [List.rev x] |> List.map List.last |> List.sum))
+printfn "Part 1: %i" (sequences |> List.sumBy (fun x -> differences [x]))
+printfn "Part 2: %i" (sequences |> List.sumBy (fun x -> differences [List.rev x]))
