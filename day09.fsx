aeb31caa4cdf8d547c7825df312e9bfd945a6194
Timo Vahtera <timo.vahtera@gmail.com>
2023-12-09 13:15:22 +0200

    improve day 9

--- day09.fsx
+++ day09.fsx
@@ -1,11 +1,9 @@
-open System
+let sequences = [ for s in System.IO.File.ReadAllLines "inputs/day09.txt" -> s |> _.Split(' ') |> Seq.map int |> Seq.toList ]
 
-let sequences = [ for s in IO.File.ReadAllLines "inputs/day09.txt" -> s |> _.Split(' ') |> Seq.map int |> Seq.toList ]
+let rec solve (accumulator : int list list) =
+    match accumulator |> List.last |> List.pairwise |> List.map (fun (a, b) -> b - a) with
+    | s when s |> List.forall ((=) 0) -> accumulator @ [s] |> List.map List.last |> List.sum
+    | s -> solve (accumulator @ [s])
 
-let rec solve (sequences : int list list) =
-    match sequences |> List.last |> List.pairwise |> List.map (fun (a, b) -> b - a) with
-    | s when s |> List.forall ((=) 0) -> sequences @ [s] |> List.map List.last |> List.sum
-    | s -> solve (sequences @ [s])
-
-printfn "Part 1: %i" (sequences |> List.sumBy (fun sequence -> solve [sequence]))
-printfn "Part 2: %i" (sequences |> List.sumBy (fun sequence -> solve [List.rev sequence]))
+sequences |> List.sumBy (fun sequence -> solve [sequence]) |> printfn "Part 1: %i"
+sequences |> List.sumBy (fun sequence -> solve [List.rev sequence]) |> printfn "Part 2: %i"
