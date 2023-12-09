62d48f8c3f9085a69dfc428f1473f72dfcbfda53
Timo Vahtera <timo.vahtera@gmail.com>
2023-12-09 13:09:05 +0200

    Improve day 9

--- day09.fsx
+++ day09.fsx
@@ -2,10 +2,10 @@ open System
 
 let sequences = [ for s in IO.File.ReadAllLines "inputs/day09.txt" -> s |> _.Split(' ') |> Seq.map int |> Seq.toList ]
 
-let rec differences (sequences : int list list) =
+let rec solve (sequences : int list list) =
     match sequences |> List.last |> List.pairwise |> List.map (fun (a, b) -> b - a) with
     | s when s |> List.forall ((=) 0) -> sequences @ [s] |> List.map List.last |> List.sum
-    | s -> differences (sequences @ [s])
+    | s -> solve (sequences @ [s])
 
-printfn "Part 1: %i" (sequences |> List.sumBy (fun x -> differences [x]))
-printfn "Part 2: %i" (sequences |> List.sumBy (fun x -> differences [List.rev x]))
+printfn "Part 1: %i" (sequences |> List.sumBy (fun sequence -> solve [sequence]))
+printfn "Part 2: %i" (sequences |> List.sumBy (fun sequence -> solve [List.rev sequence]))
