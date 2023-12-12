c2ad56f5f58367f7e7367bc49e8ab01398f80fa2
Timo Vahtera <timo.vahtera@reaktor.com>
2023-12-12 09:46:02 +0200

    Improve day 11 formatting

--- day11.fsx
+++ day11.fsx
@@ -3,34 +3,22 @@ let input =
     |> Array.mapi (fun y row -> row.ToCharArray() |> Array.mapi (fun x item -> x, y, item))
     |> Array.collect id
 
-let empties f =
-    input
-    |> f
-    |> Array.filter (snd >> Array.forall (fun (_, _, o) -> o = '.'))
-    |> Array.map fst
+let empties f = input |> f |> Array.filter (snd >> Array.forall (fun (_, _, o) -> o = '.')) |> Array.map fst
+let rows = Array.groupBy (fun (_, y, _) -> y) |> empties |> Set
+let cols = Array.groupBy (fun (x, _, _) -> x) |> empties |> Set
 
-let rows = Array.groupBy (fun (_, y, _) -> y) |> empties
-let cols = Array.groupBy (fun (x, _, _) -> x) |> empties
-
-let rec combinations n l =
-    match n, l with
-    | 0, _ -> [ [] ]
-    | _, [] -> []
-    | k, (x :: xs) -> List.map ((@) [ x ]) (combinations (k - 1) xs) @ combinations k xs
+let combinations (p: 'a array) =
+    [ for i in 0 .. p.Length-1 do
+        for j in i+1 .. p.Length-1 do
+            yield p[i], p[j] ]
 
 let distance emptyCost ((x1, y1, _), (x2, y2, _)) =
-    [ [ for x in (min x1 x2) + 1 .. (max x1 x2) -> if Array.contains x cols then emptyCost else 1UL ]
-      [ for y in (min y1 y2) + 1 .. (max y1 y2) -> if Array.contains y rows then emptyCost else 1UL ] ]
+    [ [ for x in (min x1 x2) + 1 .. (max x1 x2) -> if cols.Contains x then emptyCost else 1UL ]
+      [ for y in (min y1 y2) + 1 .. (max y1 y2) -> if rows.Contains y then emptyCost else 1UL ] ]
     |> List.collect id
     |> List.sum
 
-let galaxies =
-    input
-    |> Array.filter (fun (_, _, c) -> c = '#')
-    |> Array.toList
-    |> combinations 2
-    |> List.map (fun x -> x[0], x[1])
+let galaxies = input |> Array.filter (fun (_, _, c) -> c = '#') |> combinations
 
 galaxies |> List.map (distance 2UL) |> List.sum |> printfn "Part 1: %A"
-
 galaxies |> List.map (distance 1000000UL) |> List.sum |> printfn "Part 2: %A"
